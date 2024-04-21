e1881af3f858ef5ec01353b0629252d3e8cc5c63
agustin <agustinphysx@gmail.com>
2024-04-20 22:13:43 -0600

    Energias correctas por spin y completa, con impresiones en pantalla

--- Fisica/src/Issing_2d.c
+++ Fisica/src/Issing_2d.c
@@ -4,18 +4,16 @@
 #include <time.h>
 #include "../lib/my_math_stats.h"
 
-#define SIZE 4
+#define SIZE 8
 
-typedef struct
-{
+typedef struct{
     int up;
     int down;
     int left;
     int right;
 } neighbor;
 
-neighbor get_neighbors(int row, int col)
-{
+neighbor get_neighbors(int row, int col){
     neighbor n;
 
     n.left = (col - 1);
@@ -45,8 +43,7 @@ neighbor get_neighbors(int row, int col)
     return n;
 }
 
-int H_ij(int lattice[SIZE][SIZE], int row, int col)
-{
+int H_ij(int lattice[SIZE][SIZE], int row, int col){
     neighbor n = get_neighbors(row, col);
     int sum = 0;
 
@@ -55,42 +52,37 @@ int H_ij(int lattice[SIZE][SIZE], int row, int col)
     sum += lattice[row][n.left];
     sum += lattice[row][n.right];
 
-    return lattice[row][col] * sum;
+    return -1*lattice[row][col] * sum;
 }
 
-int H(int lattice[SIZE][SIZE])
-{
+int H(int lattice[SIZE][SIZE]){
     int sum = 0;
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
             sum += H_ij(lattice, i, j);
         }
     }
     return sum;
 }
 
-int main()
-{
+int main(){
     srand(316032629);
 
     int lattice[SIZE][SIZE] = {0};
-    int cont = 0;
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
+    int h_ij,h_tot; 
+    
+    for (int i = 0; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++){
             /*
             int spin = randnum_int(-1,1);
             while(spin == 0){
                 spin = randnum_int(-1,1);
             }
             */
-            lattice[i][j] = cont;
-            cont++;
+           lattice[i][j]=-1;
         }
     }
+    
 
     neighbor n;
 
@@ -98,7 +90,9 @@ int main()
     for(int col=0;col<SIZE;col++){
 
     n = get_neighbors(row,col);
+    h_ij = H_ij(lattice,row,col);
 
+    printf("H_ij: %d\n",h_ij);
     printf("\t [%d,%d]: %d \t \n", n.up, col, lattice[n.up][col]);
     printf("[%d,%d]: %d \t", row, n.left, lattice[row][n.left]);
     printf("[%d,%d]: %d", row, col, lattice[row][col]);
@@ -110,14 +104,14 @@ int main()
     }
     }
 
-for (int i = 0; i < SIZE; i++)
-{
-    for (int j = 0; j < SIZE; j++)
-    {
+h_tot=H(lattice);
+for (int i = 0; i < SIZE; i++){
+    for (int j = 0; j < SIZE; j++){
         printf("%d ", lattice[i][j]);
     }
     printf("\n");
 }
+printf("H_tot: %d\n",h_tot);
 
 return 0;
 }
