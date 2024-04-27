dc28ffacd700b11c82a526f2e59274698568acae
agustin <agustinphysx@gmail.com>
2024-04-27 11:04:39 -0600

    simulacion exitosa para energias en red de 128x128

--- Fisica/src/Issing_2d.c
+++ Fisica/src/Issing_2d.c
@@ -4,34 +4,20 @@
 #include <time.h>
 #include "../lib/my_math_stats.h"
 
-#define SIZE 4
+#define SIZE 128
 
 typedef struct{
-    int up;
     int down;
-    int left;
     int right;
 } neighbor;
 
 neighbor get_neighbors(int row, int col){
     neighbor n;
 
-    n.left = (col - 1);
-    if (n.left < 0)
-    {
-        n.left = SIZE - 1;
-    }
-
     n.right = (col + 1);
     if (n.right >= SIZE)
     {
         n.right = 0;
-    }
-
-    n.up = (row - 1);
-    if (n.up < 0)
-    {
-        n.up = SIZE - 1;
     }
 
     n.down = (row + 1);
@@ -47,12 +33,10 @@ int H_ij(int lattice[SIZE][SIZE], int row, int col){
     neighbor n = get_neighbors(row, col);
     int sum = 0;
 
-    //sum += lattice[n.up][col];
     sum += lattice[n.down][col];
-    //sum += lattice[row][n.left];
     sum += lattice[row][n.right];
 
-    return -1*lattice[row][col] * sum;
+    return -1*lattice[row][col]*sum;
 }
 
 int H(int lattice[SIZE][SIZE]){
@@ -63,14 +47,6 @@ int H(int lattice[SIZE][SIZE]){
         }
     }
     return sum;
-}
-
-float p_accept(int dH,int T){
-    if(dH<=0){
-        return 1.0;
-    }else{
-        return exp(-dH/T);
-    }
 }
 
 void initialize_lattice(int lattice[SIZE][SIZE]){
@@ -97,44 +73,53 @@ void print_lattice(int lattice[SIZE][SIZE]){
 }
 
 void sweep(int lattice[SIZE][SIZE], int T){
-    int h0_ij,hs_ij,dH;
+    int h0,hs,dH;
     float p,rand;
     for(int i=0;i<SIZE;i++){
         for(int j=0;j<SIZE;j++){
 
-            h0_ij = H_ij(lattice,i,j);
-            //printf("h0: %d\n",h0_ij);
-            //printf("lattice[%d][%d]: %d\n",i,j,lattice[i][j]);
-
+            h0 = H(lattice);
             lattice[i][j] = -1*lattice[i][j];
-            hs_ij = H_ij(lattice,i,j);
-            //printf("hs: %d\n",hs_ij);
+            hs = H(lattice);
 
-            dH = hs_ij - h0_ij;
-            //printf("dH:%d\n",dH);
+            dH = hs - h0;
 
-            p = p_accept(dH,T);
-            rand = randnum(0,1);
-            //printf("p:%f\n",p);
-            //printf("rand:%f\n",rand);
-            if (p!=1.0 && rand > p){
-                lattice[i][j] = -1*lattice[i][j];
+            if(dH>0){
+                p = exp(-1*(float)dH/T);
+                rand = randnum(0,1);
+                if(rand>p){
+                    lattice[i][j] = -1*lattice[i][j];
+                }
             }
-            //printf("lattice[%d][%d]: %d\n\n",i,j,lattice[i][j]);
+            
         }
     }
 }
 
 int main(){
-    srand(316032629);
+    srand(time(NULL));
+    int lattice[SIZE][SIZE];
 
-    //int lattice[SIZE][SIZE] = {{1,-1,1,-1},{-1,1,-1,1},{1,-1,1,-1},{-1,1,-1,1}};
-    //int lattice[SIZE][SIZE] = {{1,1,1,1},{1,1,1,1},{1,1,1,1},{1,1,1,1}};
-    int lattice[SIZE][SIZE] = {{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1}};
+    initialize_lattice(lattice);
+    //print_lattice(lattice);
+
+    //print_lattice(lattice);
+
+    //printf("H: %d\n",H(lattice));
     
-    print_lattice(lattice);
-    printf("H: %d\n",H(lattice));
+    char ruta[50];
+    sprintf(ruta,"../../Notebooks_Py/Datos/issing_2d.csv");
+    FILE *archivo = fopen(ruta, "w"); 
+    fprintf(archivo,"sweep,H\n");
 
-   
+    for(int i =0;i<100;i++){
+        sweep(lattice,2);
+        int H_tot = H(lattice); 
+        //printf("H%d: %d\n",i,H_tot);
+        fprintf(archivo, "%d,%d\n", i,H_tot);
+    }
+
+    fclose(archivo);
+    
 return 0;
 }
