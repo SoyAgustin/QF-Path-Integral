b9f0fa0e13d7ebee65aab80fa536115c4902a611
agustin <agustinphysx@gmail.com>
2024-08-25 14:23:20 -0600

    a = 10/SIZE, SE_cold_hot y acc_rates

--- Fisica/src/oscillator_final.c
+++ Fisica/src/oscillator_final.c
@@ -2,29 +2,34 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include <string.h>
 #include "../lib/my_math_stats.h"
 
 #define SIZE 10
+#define a (10.0/SIZE)
+#define lambda 0.0
+double epsilon = 0.3;
 
-void initialize_x(float x[SIZE],int start){
+// iteraciones de Humberto 4000000
+#define REPETITIONS 101000//101000//510000
+#define TERMALIZATION 1000//1000//10000
+#define STEPS_TO_MEASURE 10
+#define MEASURES ((REPETITIONS-TERMALIZATION)/STEPS_TO_MEASURE)
+
+
+void initialize_x(double x[SIZE],int start){
     if (start==0){
         for (int i = 0; i < SIZE; i++){
-            x[i]=0;
+            x[i]=0.0;
     }
     }else{
         for (int i = 0; i < SIZE; i++){
-            if(i==SIZE-1){
-                x[SIZE-1]=x[0]; //Condiciones periodicas
-            }else{
-                float pos = randnum(-1,1); //reales entre -1 y 1
-                x[i]=pos;
+            double pos = randnum(-1.0,1.0); //reales entre -1 y 1
+            x[i]=pos;
             } 
-        }
-    }
+        } 
 }
 
-void print_x(float x[SIZE]){
+void print_x(double x[SIZE]){
     
     for (int i = 0; i < SIZE; i++){
         
@@ -42,12 +47,12 @@ void print_x(float x[SIZE]){
     
 }
 
-float S_E(float x[SIZE],float a,float lambda){
-    float sum=0.0;
+double S_E(double x[SIZE]){
+    double sum=0.0;
 
     for(int i =0; i<SIZE; i++){
         if(i==SIZE-1){
-            sum+=0.5*pow(x[i],2)+lambda*pow(x[i],4.0);
+            sum+=0.5 * pow((x[0] - x[i]) / a, 2)+0.5*pow(x[i],2)+lambda*pow(x[i],4.0); //Condición periodica x[N+1] = x[0]
         }else{
             sum += 0.5 * pow((x[i+1] - x[i]) / a, 2) + 0.5 * pow(x[i], 2) + lambda * pow(x[i], 4.0);
         } 
@@ -55,9 +60,8 @@ float S_E(float x[SIZE],float a,float lambda){
     return a*sum;
 }
 
-
-float dSE(float xl, float x0, float xr, float xf, float a, float lambda){
-    float sum=0.0;
+double dSE(double xl, double x0, double xr, double xf){
+    double sum=0.0;
     sum += (pow(xr - xf, 2) - pow(xr - x0, 2)) /(2*a*a);
     sum += (pow(xf - xl, 2) - pow(x0 - xl, 2)) /(2*a*a);
     sum += 0.5*(pow(xf,2)-pow(x0,2));
@@ -65,175 +69,166 @@ float dSE(float xl, float x0, float xr, float xf, float a, float lambda){
     return a*sum;
 }
 
-/*
-float sweep(float x[SIZE],float epsilon, float a,float lambda){
-    float xl,x0,xr,xf=0;
-    float rho = 0.0;
-    int acc_rate = 0;
+double sweep(double x[SIZE],double epsilon){
+    double dse,xp = 0.0;
+    double p,rand,rho = 0.0;
+    double acc_rate = 0.0;
 
     for(int i=0;i<SIZE;i++){
-        if(i==SIZE-1){
-            xr = x[0];
-            x0 = x[i];
-            xl = x[i-1];
-        }
-        else if(i==0){
-            xr = x[i+1];
-            x0 = x[i];
-            xl = x[SIZE-1];
-        }
-        else{
-            xr = x[i+1];
-            x0 = x[i];
-            xl = x[i-1];
-        }
         
         rho = randnum(-epsilon,epsilon);
-        xf = x0+rho; //Hacemos un cambio en x0 
-
-        float dS = dSE(xl,x0,xr,xf,a,lambda); // Se calcula el cambio de acción
         
-        if(dS<=0){
-           x[i] = xf;  //Se acepta el cambio
-           acc_rate++;
+        xp = x[i]+rho; //Hacemos un cambio
+        
+        dse = dSE(x[(i-1)%SIZE],x[i],x[(i+1)%SIZE],xp); //calculamos el cambio de accion
+
+        if(dse<=0){ //si es menor o igual que cero se realiza el cambio
+            acc_rate++;
+            x[i] = xp;
         }
-        else{
-            float p = exp(-dS); //probabilidad de rechazo
-            float rand = randnum(0,1); 
-            if(rand<=p){ 
-                x[i] = xf; //Se acepta el cambio
+
+        if(dse>0){ //Si es mayor que cero metropolis
+            p = exp(-dse);
+            rand = randnum(0.0,1.0);
+            if(rand>p){ //Si rand es mayor que p, se rechaza el cambio
+                //no se hace nada con el array
+            }else{ //si es menor o igual que p se realiza el cambio
                 acc_rate++;
+                x[i] = xp;
             }
         }
-    }
-
-    return (float)acc_rate/(SIZE);
-}
-*/
-
-float sweep(float x[SIZE],float epsilon, float a,float lambda){
-    float SE_0,SE_s,dse;
-    float p,rand,rho;
-    float acc_rate = 0.0;
-
-    for(int i=0;i<SIZE;i++){
-        if(i==SIZE-1){
-            x[i]=x[0];// condicion peridica
-        }
-        else{
-            rho = randnum(-epsilon,epsilon);
-            
-            float xp = x[i]+rho;
-            //x[i] = x[i]+rho;//Hacemos un cambio 
-            
-            dse = dSE(x[i-1%SIZE],x[i],x[i+1%SIZE],xp,a,lambda);
-            if(dse<=0){
-                acc_rate++;
-                x[i] = x[i]+rho;
-            }
-            if(dse>0){ //Si es mayor que cero 
-                p = exp(-1*dse);
-                rand = randnum(0,1);
-                if(rand>p){ //Si es mayor que p, se rechaza el cambio
-
-                }else{
-                    acc_rate++;
-                    x[i] = x[i]+rho;
-                }
-            }
-        }  
    
     }
-    return acc_rate/(SIZE-1);
+    return acc_rate/(SIZE);
 }
 
-
-#define REPETITIONS 101000//101000//510000
-#define TERMALIZATION 1000//1000//10000
-#define STEPS_TO_MEASURE 10
-#define MEASURES ((REPETITIONS-TERMALIZATION)/STEPS_TO_MEASURE)
-int main(){
-
-    srand(time(NULL));
-    float x[SIZE];
-    int start = 1; //Hot: 1, Cold: 0
+void test_SE_dse(double x_0, double xp){
+    double x0[SIZE]={5,1,2,4,5,x_0,7,8,9,2};
+    double x1[SIZE]={5,1,2,4,5,xp,7,8,9,2};
     
-    initialize_x(x,start);
-    int steps_to_measure = 10;
-
-    float epsilon = 0.25;
-    float a=0.1;
-    float lambda = 0;
+    //double lambda = 1;
     
-    float acc_rate = 0.0;
-    float sw;
-    float x0=0;
-    float x5=0;
-    float x9=0;
-    float SE=0;
-    for (int i = 0; i < REPETITIONS; i++){
-        sw = sweep(x,epsilon,a,lambda);
-        if (i>=TERMALIZATION && i%steps_to_measure==0){
-             acc_rate+=sw;
-             SE+=S_E(x,a,lambda);
-             x0+=x[0]*x[0];
-             x5+=x[5]*x[5];
-             x9+=x[9]*x[9];
-        }
-    }
 
-    printf("acc_rate: %f\n",acc_rate/MEASURES);
-    printf("SE: %f\n",SE/MEASURES);
-    printf("x0: %f\n",x0/(MEASURES));
-    printf("x5: %f\n",x5/MEASURES);
-    printf("x9: %f\n",x9/MEASURES);
-
-
-    //SE 
-/*
-    float x0[SIZE];
-    float x1[SIZE];
-    //int start = 1; //Hot: 1, Cold: 0
-    initialize_x(x0,0);
-    initialize_x(x1,1);
-    int steps_to_measure = 10;
-
-    float epsilon = 0.2;
-    float a=0.05;
-    float lambda = 1;
-    
-    
-    printf("SE_cold,SE_hot\n");
-    for (int i = 0; i < REPETITIONS; i++){
-        sweep(x0,epsilon,a,lambda);
-        sweep(x1,epsilon,a,lambda);
-        if (i>=TERMALIZATION && i%steps_to_measure==0){
-            printf("%f,%f\n",S_E(x0,a,lambda),S_E(x1,a,lambda));   
-        }
-    }
-*/
-
-    //Information
-   // printf("MEASURES: %d , epsilon: %0.2f , lambda: %0.2f, a: %0.2f \n",MEASURES,epsilon,lambda,a);
-
-/*
-     //CONTROL PASS
-    srand(time(NULL));
-    float x0[SIZE]={5,1,2,4,5,6,7,8,9,2};
-    float x1[SIZE]={5,1,2,4,5,8,7,8,9,2};
-    
-    float a = 0.05;
-    float lambda = 1;
-
-    float s0  = S_E(x0,a,lambda);
-    float s1  = S_E(x1,a,lambda);
+    double s0  = S_E(x0);
+    double s1  = S_E(x1);
 
     printf("s0: %f\n",s0);
     printf("s1: %f\n",s1);
 
     printf("s1-s0: %f\n",s1-s0);
-    printf("s0-s1: %f\n",dSE(x0[4],x0[5],x0[6],x1[5],a,lambda));
-*/
+    printf("s1-s0: %f\n",dSE(x0[4],x0[5],x0[6],x1[5]));
+}
+
+void SE_cold_hot(double epsilon){
+    double x0[SIZE] = {0.0};
+    double x1[SIZE] = {0.0};
+    //int start = 1; //Hot: 1, Cold: 0
+    initialize_x(x0,0);
+    initialize_x(x1,1);
+    
+    printf("SE_cold,SE_hot\n");
+    for (int i = 0; i < REPETITIONS; i++){
+        sweep(x0,epsilon);
+        sweep(x1,epsilon);
+        if (i>=TERMALIZATION && i%STEPS_TO_MEASURE==0){
+            printf("%f,%f\n",S_E(x0),S_E(x1));   
+        }
+    }
+}
+
+void acc_rate_core(double epsilon){
+    double x[SIZE] = {0.0};
+    int start = 1;
+    initialize_x(x,start);
+    double acc_rates[MEASURES] = {0.0};
+    double sweep_i = 0.0;
+    double mean_se[2] = {0.0};
+    double mean_acc_rates = 0.0;
+    double se = 0.0;
+    
+    int index = 0;
+    for (int i = 0; i < REPETITIONS; i++){
+        sweep_i = sweep(x,epsilon);
+        if (i>=TERMALIZATION && i%STEPS_TO_MEASURE==0){
+              acc_rates[index]+=sweep_i; 
+              index++;
+        }
+    }
+
+    mean_acc_rates = mean(MEASURES,acc_rates);
+    se = error(MEASURES,acc_rates);
+    printf("%.1f,%.6f,%.6f\n",epsilon,mean_acc_rates,se);
+}
+
+void acc_rates(float step, int N){
+    double epsilon = 0.0;
+    printf("epsilon,mean,se\n");
+    for(int i = 0; i<=N; i++){
+        acc_rate_core(epsilon);
+        epsilon+=step;
+    }
+}
+
+int main(){
+    srand(time(NULL));
+
+/*
+    double x[SIZE];
+    int start = 1; //Hot: 1, Cold: 0
+    initialize_x(x,start);
+    
+    double acc_rate = 0.0;
+    double sw;
+    double x0_2=0;
+    double x5_2=0;
+    double x9_2=0;
+    double SE[MEASURES] = {0};
+    int j=0;
+    for (int i = 0; i < REPETITIONS; i++){
+        sw = sweep(x,epsilon);
+        if (i>=TERMALIZATION && i%STEPS_TO_MEASURE==0){
+             acc_rate+=sw;
+             SE[j]=S_E(x);
+             x0_2+=x[0]*x[0];
+             x5_2+=x[5]*x[5];
+             x9_2+=x[9]*x[9];
+             j++;
+        }
+    }
+
+    printf("acc_rate: %f\n",acc_rate/MEASURES);
+    printf("<SE>: %f, err: %f\n",mean(MEASURES,SE),error(MEASURES,SE));
+    printf("x0: %f\n",x0_2/(MEASURES));
+    printf("x5: %f\n",x5_2/MEASURES);
+    printf("x9: %f\n",x9_2/MEASURES);
+    */
+    
+
+    //Information
+    //printf("MEASURES: %d , epsilon: %0.2f , lambda: %0.2f, a: %0.2f \n",MEASURES,epsilon,lambda,a);
+
+    /*
+    //Test de acción y dSE
+    srand(time(NULL));
+    double x_0 = 5.0;
+    double xp = 6.0; 
+    test_SE_dse(x_0,xp);
+    */
+
+    /*
+    //acc_rates
+    float step = 0.1;
+    int N = 20;
+    acc_rates(step,N);
+    */
+
+
+    /*
+    //SE_cold,SE_hot
+    double epsilon = 0.7;
+    double lambda = 0;
+    SE_cold_hot(epsilon,lambda);
+    */
 
     return 0;
 }
