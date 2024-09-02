bd661317bc3bfb6b671bb39c6dcb0ddbff44ead5
agustin <agustinphysx@gmail.com>
2024-09-02 07:09:17 -0600

    commit lunes 2 sep

--- Fisica/src/oscillator_final.c
+++ Fisica/src/oscillator_final.c
@@ -4,14 +4,17 @@
 #include <time.h>
 #include "../lib/my_math_stats.h"
 
+
+#define N 20
+double epsilon = 0.55;
+const double lambda  = 1.0;
+
 #define SIZE 10
-#define N 150
-double a = ((double)SIZE/N);
-double lambda  = 1.0;
-double epsilon = 0.3;
+const double a = ((double)SIZE/N);
+
 
 // iteraciones de Humberto 4000000
-#define REPETITIONS 101000//101000//510000
+#define REPETITIONS 401000//101000//510000
 #define TERMALIZATION 1000//1000//10000
 #define STEPS_TO_MEASURE 10
 #define MEASURES ((REPETITIONS-TERMALIZATION)/STEPS_TO_MEASURE)
@@ -61,12 +64,12 @@ double S_E(double x[N]){
     return a*sum;
 }
 
-double dSE(double xl, double x0, double xr, double xf){
+double dSE(double xl, double x0, double xr, double xp){
     double sum=0.0;
-    sum += (pow(xr - xf, 2) - pow(xr - x0, 2)) /(2*a*a);
-    sum += (pow(xf - xl, 2) - pow(x0 - xl, 2)) /(2*a*a);
-    sum += 0.5*(pow(xf,2)-pow(x0,2));
-    sum += lambda*(pow(xf,4.0)-pow(x0,4.0));
+    sum += (pow(xr - xp, 2) - pow(xr - x0, 2)) /(2*a*a);
+    sum += (pow(xp - xl, 2) - pow(x0 - xl, 2)) /(2*a*a);
+    sum += 0.5*(pow(xp,2)-pow(x0,2));
+    sum += lambda*(pow(xp,4.0)-pow(x0,4.0));
     return a*sum;
 }
 
@@ -178,21 +181,26 @@ void corr(){
     double x[N] = {0.0};
     int start = 1;
     initialize_x(x,start);
-    double corr_measures[MEASURES][N] = {0.0};
-    double corr_mean[2][N]= {0.0};
+    double corr_measures[MEASURES][N+1];
+    double corr_mean[2][N+1];
 
     int ind = 0;
     for(int i = 0; i < REPETITIONS; i++){
         sweep(x,epsilon);
         if (i>=TERMALIZATION && i%STEPS_TO_MEASURE==0){
-            for(int j = 0; j<N; j++){
-                corr_measures[ind][j] = x[0]*x[j];
+            for(int j = 0; j<=N; j++){
+                if(j==N){
+                    corr_measures[ind][j] = corr_measures[ind][0];
+                }
+                else{
+                    corr_measures[ind][j] = x[0]*x[j];
+                }
             }
             ind++;
         }
     }
 
-    for(int i = 0; i < N; i++) {
+    for(int i = 0; i <= N; i++) {
         double temp[MEASURES];
         for (int j = 0; j < MEASURES; j++) {
             temp[j] = corr_measures[j][i];
@@ -202,8 +210,8 @@ void corr(){
     }
 
     for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%f ", corr_mean[i][j]);
+        for (int j = 0; j <= N; j++) {
+            printf("%f, ", corr_mean[i][j]);
         }
         printf("\n");
     }
@@ -213,34 +221,29 @@ void corr(){
 int main(){
     srand(time(NULL));
 
-    double x[N];
-    int start = 1; //Hot: 1, Cold: 0
-    initialize_x(x,start);
+    // double x[N];
+    // int start = 1; //Hot: 1, Cold: 0
+    // initialize_x(x,start);
     
-    double acc_rate = 0.0;
-    double sw=0.0;
-    double x5[MEASURES]={0.0};
-    double SE[MEASURES] = {0.0};
-    double x_wlambda[MEASURES] = {0.0};
-    int j=0;
-    for (int i = 0; i < REPETITIONS; i++){
-        sw = sweep(x,epsilon);
-        if (i>=TERMALIZATION && i%STEPS_TO_MEASURE==0){
-             acc_rate+=sw;
-             SE[j]=S_E(x);
-             //x5[j]=x[0]*x[0];
-             x_wlambda[j]=pow(x[5],2)+3*lambda*pow(x[5],4); 
-             j++;
-        }
-    }
+    // double acc_rate = 0.0;
+    // double sw=0.0;
 
-    printf("acc_rate: %f\n",acc_rate/MEASURES);
-    //printf("<SE>: %f, err: %f\n",mean(MEASURES,SE),error(MEASURES,SE));
-    //printf("<E_0> = <x_5^2>: %f, err: %f\n",mean(MEASURES,x5),error(MEASURES,x5));
-    printf("<E_0> = <x_5^2>+3lambda*x_5^4: %f, err: %f\n",mean(MEASURES,x_wlambda),error(MEASURES,x_wlambda));
-    
-    
+    // double E_0[MEASURES];
+    // int j=0;
+    // for (int i = 0; i < REPETITIONS; i++){
+    //     sw = sweep(x,epsilon);
+    //     if (i>=TERMALIZATION && i%STEPS_TO_MEASURE==0){
+    //          acc_rate+=sw;
+    //          E_0[j]=pow(x[5],2)+3*lambda*pow(x[5],4); 
+    //          j++;
+    //     }
+    // }
 
+    // printf("acc_rate: %f\n",acc_rate/j);
+    // printf("E_0: %f\nstderr: %f\n",mean(MEASURES,E_0),error(MEASURES,E_0));
+    
+    
+    corr();
     //Information
     printf("MEASURES: %d , epsilon: %0.2f , lambda: %0.2f, a: %0.2f \n",MEASURES,epsilon,lambda,a);
 
