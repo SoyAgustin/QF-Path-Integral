5ce14005a30e1807e38ee9c28259057a230d6933
agustin <agustinphysx@gmail.com>
2024-10-01 18:31:05 -0600

    script de bash para correr todas las simulaciones y guardarlas

---
+++ Fisica/src/run_oscillator.sh
@@ -0,0 +1,7 @@
+time ../bin/E_l0.exe > ../../Notebooks_Py/Datos/oscillatorF/gs_l0.csv
+time ../bin/E_l05.exe > ../../Notebooks_Py/Datos/oscillatorF/gs_l05.csv
+time ../bin/E_l1.exe > ../../Notebooks_Py/Datos/oscillatorF/gs_l1.csv
+
+time ../bin/corr_n100_l0.exe > ../../Notebooks_Py/Datos/oscillatorF/corr_l0.csv
+time ../bin/corr_n100_l05.exe > ../../Notebooks_Py/Datos/oscillatorF/corr_l05.csv
+time ../bin/corr_n100_l1.exe > ../../Notebooks_Py/Datos/oscillatorF/corr_l1.csv
