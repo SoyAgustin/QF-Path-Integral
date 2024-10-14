a35f8784a76a9f3dc1f4f33765c64ce7642ae654
Agustin <apineda@metricas.mx>
2024-10-14 14:11:02 -0600

    pequenna modificacion para lo del coeficiente del potencial

--- Fisica/src/mod.f90
+++ Fisica/src/mod.f90
@@ -4,7 +4,7 @@ integer, parameter :: dp = selected_real_kind(15, 307)
 real(dp) , allocatable :: x(:),xi_squared_arr(:),xi_quart_arr(:), E_array(:)
 real(dp) :: a,epsilon,rand,extrand,SE,SE1,rho,deltaSE,prob,E_cont,E_cont_2
 integer :: i,j,k,acc,measures_cont,n,sweeps,measures,steps
-real(dp) :: lambda, E_mean, E_error
+real(dp) :: lambda,b, E_mean, E_error
 real(dp) :: acc_rate
 integer :: n_arr(9), M
 real(dp) :: epsilon_arr(9)
@@ -15,16 +15,17 @@ epsilon_arr = (/0.75, 0.65, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25,0.2/) !lambda = 0
 !epsilon_arr = (/0.65, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25,0.2/) !lambda = 0.5
 !epsilon_arr = (/0.6, 0.55, 0.5, 0.45, 0.4, 0.35, 0.3, 0.25,0.2/) !lambda = 1
 
-write(*,*) "E_0, err, acc_rate"
+! write(*,*) "E_0, err, acc_rate"
 
-do k = 1, 7
+do k = 1, 1
 
 	n = n_arr(k)
 	epsilon = real(epsilon_arr(k),dp)
 	!n = 100
 	!epsilon = 0.35
 	
-	lambda = 0.0d0
+	lambda = 1.0d0
+	b = -1.0d0 !parámetro del término cuadrático del potencial 
 	
 	a = 10.0d0/real(n,dp)
 
@@ -35,9 +36,9 @@ do k = 1, 7
 
 	measures_cont=1
 
-	sweeps = 100000000
-	termalization = 100000
-	steps = 1000
+	sweeps = 101000
+	termalization = 1000
+	steps = 10
 	measures = int( (sweeps - termalization)/steps )
 
 	allocate(x(n))
@@ -57,10 +58,10 @@ do k = 1, 7
 		if(i .ge. termalization .and. mod(i,steps) .eq. 0) then
 
 			!!E_array(measures_cont) = x(1)**2.0d0 + 3.0d0 * lambda * x(1)**4.0d0
-			E_cont = E_cont + x(1)**2.0d0 + 3.0d0 * lambda * x(1)**4.0d0
-			E_cont_2 = E_cont_2 + (x(1)**2.0d0 + 3.0d0 * lambda * x(1)**4.0d0)**2.0d0
+			! E_cont = E_cont + b*x(1)**2.0d0 + 3.0d0 * lambda * x(1)**4.0d0
+			! E_cont_2 = E_cont_2 + (b*x(1)**2.0d0 + 3.0d0 * lambda * x(1)**4.0d0)**2.0d0
 
-			!print *, x(1)**2.0d0 + 3.0d0 * lambda * x(1)**4.0d0
+			print *, b*x(1)**2.0d0 + 3.0d0 * lambda * x(1)**4.0d0
 			
 			
 			measures_cont=measures_cont+1
@@ -73,14 +74,14 @@ do k = 1, 7
 	!call mean_error(E_array, measures,E_mean,E_error)
 	!deallocate(E_array)
 
-	acc_rate = acc /(real(n,dp)*sweeps)
+	! acc_rate = acc /(real(n,dp)*sweeps)
 
-	E_mean = E_cont / M
+	! E_mean = E_cont / M
 
-	E_error = (E_cont_2 - M*(E_mean)**2.0d0)/(M-1.0d0)
-	E_error = sqrt(E_error/(M))
+	! E_error = (E_cont_2 - M*(E_mean)**2.0d0)/(M-1.0d0)
+	! E_error = sqrt(E_error/(M))
 
-	write(*,"(F10.5,A,F10.5,A,F10.5)") E_mean,",", E_error,",", acc_rate
+	! write(*,"(F10.5,A,F10.5,A,F10.5)") E_mean,",", E_error,",", acc_rate
 	deallocate(x)
 
 end do 
@@ -96,11 +97,11 @@ function S_E(arreglo) result(resultado)
     resultado = 0.0d0
     
     do p = 1, (n-1)
-        resultado = resultado + 0.5d0*((arreglo(p+1) - arreglo(p))/a)**2.0d0 + 0.5d0*arreglo(p)**2.0d0 + lambda*arreglo(p)**4.0d0
+        resultado = resultado + 0.5d0*((arreglo(p+1) - arreglo(p))/a)**2.0d0 + 0.5d0*b*arreglo(p)**2.0d0 + lambda*arreglo(p)**4.0d0
     end do
 
     ! Sumar el término adicional x_f^2
-    resultado = resultado + 0.5d0*((arreglo(1) - arreglo(n))/a)**2.0d0 + 0.5d0*arreglo(n)**2.0d0 + lambda*arreglo(n) ** 4.0d0
+    resultado = resultado + 0.5d0*((arreglo(1) - arreglo(n))/a)**2.0d0 + 0.5d0*b*arreglo(n)**2.0d0 + lambda*arreglo(n) ** 4.0d0
 
     ! Multiplicar por a
     resultado = a * resultado
@@ -156,54 +157,11 @@ function delta_S(xl, xi, xr, xp) result(sum)
     
     sum = 0.0
     sum = sum + ( (xr - xp)**2 - (xr - xi)**2 + (xp - xl)**2 - (xi - xl)**2 ) / (2*a**2)
-    sum = sum + 0.5*(xp**2-xi**2) + lambda*(xp**4-xi**4)
+    sum = sum + 0.5*b*(xp**2-xi**2) + lambda*(xp**4-xi**4)
     sum = a*sum
 
-  end function delta_S
+end function delta_S
 
-subroutine sweeep2(x)
-
-	real(dp), intent(inout) :: x(:)
-	real(dp) :: xl, xr, xp
-
-	do j=1,n
-		
-		
-		call Random_Number(rand)
-
-		rho=epsilon*(2*rand-1)
-
-		xp = x(j) + rho
-
-        xl = x(mod(j-1,n))
-        xr = x(mod(j+1,n))
-		
-		deltaSE= delta_S(xl, x(j), xr, xp)
-
-		if(deltaSE .le. 0 ) then
-	
-			acc = acc + 1 !Se acepta el cambio
-			x(j) = xp
-	
-		else if(deltaSE .gt. 0) then
-
-			prob=exp(-deltaSE)
-			call Random_Number(rand)
-
-			if(rand .lt. prob ) then
-		
-				acc = acc + 1 !Se acepta el cambio
-				x(j) = xp
-
-			else 
-
-			end if
-
-		end if
-
-
-	end do
-end subroutine sweeep2
 
 subroutine sweeep3(x)
     real(dp), intent(inout) :: x(:)
